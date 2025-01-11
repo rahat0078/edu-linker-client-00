92c3f75973bb314c70c676df26c5044abe2f928d
rahat0078 <rarahat1710@gmail.com>
2025-01-11 14:07:03 +0600

    about us page design completed

---
+++ src/pages/AboutUs/AboutHero.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutHero = () => {
+    return (
+        <div className='py-16 bg-gray-50 h-96'>
+            
+        </div>
+    );
+};
+
+export default AboutHero;
--- src/pages/contact/ContInfo.jsx
+++ src/pages/contact/ContInfo.jsx
@@ -3,7 +3,7 @@ import { FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
 
 const ContInfo = () => {
     return (
-            <section className="py-16 bg-gray-50">
+            <section className="py-16">
                 <div className="container mx-auto">
                     <h2 className="text-3xl font-bold text-center mb-6">Contact Information</h2>
                     <p className="text-center text-gray-600 mb-10">
@@ -11,33 +11,33 @@ const ContInfo = () => {
                     </p>
                     <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center lg:grid-cols-4 gap-6">
                         {/* Email */}
-                        <div className="card shadow-lg bg-white p-6 text-center">
+                        <div className="card shadow-lg border border-gray-300 p-6 text-center">
                             <FaEnvelope className="text-[#4662B2] text-4xl mx-auto mb-4" />
                             <h3 className="text-xl font-semibold mb-2">Email Us</h3>
-                            <p className="text-gray-600 mb-4">support@edulinker.com</p>
+                            <p className="text-gray-500 mb-4">support@edulinker.com</p>
                             <a
                                 href="mailto:support@edulinker.com"
-                                className="btn btn-sm bg-[#4662B2] text-white hover:text-black"
+                                className="btn btn-sm bg-[#4662B2] hover:bg-[#4662B2] text-white hover:text-black"
                             >
                                 Send an Email
                             </a>
                         </div>
 
                         {/* Phone */}
-                        <div className="card shadow-lg bg-white p-6 text-center">
+                        <div className="card shadow-lg p-6 text-center border border-gray-300">
                             <FaPhone className="text-[#4662B2] text-4xl mx-auto mb-4" />
                             <h3 className="text-xl font-semibold mb-2">Call Us</h3>
                             <p className="text-gray-600 mb-4">+123-456-7890</p>
                             <a
                                 href="tel:+1234567890"
-                                className="btn btn-sm bg-[#4662B2] text-white hover:text-black"
+                                className="btn btn-sm bg-[#4662B2] hover:bg-[#4662B2] text-white hover:text-black"
                             >
                                 Call Now
                             </a>
                         </div>
 
                         {/* Address */}
-                        <div className="lg:col-span-2 col-span-1 rounded-2xl flex justify-between shadow-lg bg-white">
+                        <div className="lg:col-span-2 col-span-1 rounded-2xl flex justify-between shadow-lg border border-gray-300">
                             <div className=" p-6 text-center">
                                 <FaMapMarkerAlt className="text-[#4662B2] text-4xl mx-auto mb-4" />
                                 <h3 className="text-xl font-semibold mb-2">Visit Us</h3>
@@ -47,7 +47,7 @@ const ContInfo = () => {
                                 <a
                                     target="_blank"
                                     href="https://maps.app.goo.gl/W5ebxHmcapnJYbwP7"
-                                    className="btn btn-sm bg-[#4662B2] text-white hover:text-black w-full"
+                                    className="btn btn-sm bg-[#4662B2] hover:bg-[#4662B2] text-white hover:text-black w-full"
                                 >
                                     View on Map
                                 </a>
